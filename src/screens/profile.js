cca452ba1687e86f446860ba52967fa2354f7d14
asteroidfive <skyytles99@gmail.com>
2022-12-03 16:41:13 -0500

    edit profile page

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -10,7 +10,30 @@ const Profile = (props) => {
 
   return (
     <div>
-      <h1>Profile Page</h1>
+      <h1>Edit Profile</h1>
+      <h5>Name:</h5>
+      <input type="text"/>
+      <h5>Email:</h5>
+      <input type="text"/>
+      <h5>Username:</h5>
+      <input type="text"/>
+      <h5>Ingredients:</h5>
+      <select>
+        <option value=""></option>
+        <option value="rice">Rice</option>
+        <option value="beef">Beef</option>
+        <option value="cabbage">Cabbage</option>
+        <option value="milk">Milk</option>
+      </select>
+      <h5>Dietary Restrictions: dropdown menu</h5>
+      <select>
+        <option value=""></option>
+        <option value="vegetarian">Vegetarian</option>
+        <option value="pescatarian">Pescatarian</option>
+        <option value="vegan">Vegan</option>
+        <option value="gluten-free">Gluten-Free</option>
+      </select>
+      <h5>Recipes: list of recipes</h5>
       <input type="button" value="Logout" onClick={logoutHandler} />
     </div>
   )
