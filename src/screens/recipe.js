bd77ac0fb90fd0dd4bb614516c824c16dc7105d3
asteroidfive <skyytles99@gmail.com>
2022-12-13 13:01:07 -0500

    you can now save recipes

--- src/screens/recipe.js
+++ src/screens/recipe.js
@@ -20,7 +20,9 @@
 //     }
 
 import React, { useEffect, useState } from 'react';
+import { getUser } from '../service/AuthService';
 import {useLocation} from "react-router-dom";
+import axios from 'axios';
 import '../css/allpages.css'
 import '../css/recipe.css'
 import { SaveButton } from '../components/navbarElements';
@@ -31,9 +33,15 @@ export default function Recipe() {
   const location = useLocation();
   const meal = location.state.meal
 
+  const user = getUser();
+  const username = user !== 'undefined' && user ? user.username : '';
+
+  const updateAPIURL = 'https://5v7ysjln6j.execute-api.us-east-1.amazonaws.com/beta/profileinfo';
+
   var [mealData, setMealData] = useState({});
   const [apiKey, setKey] = useState('/information?apiKey=e0ba8f96837748dc9473f52b42c3b8a8')
   const [baseSearchURL, setBaseURL] = useState('https://api.spoonacular.com/recipes/')
+  const [message, setMessage] = useState(null);
   var [someLink, setLink] = useState('test')
 
   useEffect(() => {
@@ -55,6 +63,89 @@ export default function Recipe() {
     }
   }
 
+  async function getRecipes()  {
+    setLink(someLink = (baseSearchURL + meal.id + apiKey))
+     try {
+      const response = await fetch(someLink)
+      const json = await response.json()
+      setMealData(json)
+
+    } catch (error) {
+      console.error(error)
+    } finally {
+      // setLoading(false)
+    }
+  }
+
+  async function getSaved() {
+
+    var url = 'https://5v7ysjln6j.execute-api.us-east-1.amazonaws.com/beta/profileinfo?username='
+    url = url + username
+
+    axios.get(url).then(response => {
+      updateSaved(response.data.saved);
+      setMessage('Info Updated');
+    }).catch(error => {
+      if (error.response.status === 401) {
+        setMessage(error.response.data.message);
+      } else {
+        setMessage('sorry....the backend server is down!! please try again later');
+      }
+  })
+    
+  }
+
+  async function updateSaved(list) {
+    list.push(mealData.id)
+    list = [...new Set(list)];
+
+
+    console.log(list)
+    const requestBody = {
+      username: username,
+      updateKey: "saved",
+      updateValue: list
+    }
+
+    axios.patch(updateAPIURL, requestBody).then(response => {
+      setMessage('Info Updated');
+    }).catch(error => {
+      if (error.response.status === 401) {
+        setMessage(error.response.data.message);
+      } else {
+        setMessage('sorry....the backend server is down!! please try again later');
+      }
+  })
+    // updateRating(response.data.saved);
+  }
+
+  // function updateRating(list) {
+  //   let dropdown = document.getElementById("rating")
+  //   let rating = dropdown.value
+  //   let id = mealData.id
+
+  //   list.push()
+  //   const update = {
+  //     id: rating
+  //   }
+
+  //   const requestBody = {
+  //     username: username,
+  //     updateKey: "rated",
+  //     updateValue: update
+  //   }
+
+  //   axios.patch(updateAPIURL, requestBody).then(response => {
+  //     setMessage('Info Updated');
+  //   }).catch(error => {
+  //     if (error.response.status === 401) {
+  //       setMessage(error.response.data.message);
+  //     } else {
+  //       setMessage('sorry....the backend server is down!! please try again later');
+  //     }
+  // })
+  // }
+
   var desc = meal.summary
   desc = desc.replace(/\s*\<.*?\>\s*/g, ' ');
   //desc = desc.split('.')[0] + '.';
@@ -72,7 +163,8 @@ export default function Recipe() {
       </div>
       <img className="img" src={mealData.image} alt={mealData.title}/>
       <br/>
-      <SaveButton>Save Recipe</SaveButton>
+      <button onClick={() => getSaved()}>Save Recipe</button>
+      {/* <SaveButton>Save Recipe</SaveButton> */}
         <select id="rating">
           <option value="">Select Rating</option>
           <option value="1">Rating 1/5</option>
